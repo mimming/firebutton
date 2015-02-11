ff86af531d42d60c6c71e8d91489a8f81624ca13
Jenny Tong <jen@mimming.com>
2015-02-11 05:09:29 +0000

    added hippo

---
+++ hippo.js
@@ -0,0 +1,33 @@
+var five = require('johnny-five');
+var Firebase = require("firebase");
+
+var board = new five.Board();
+
+var firebaseRef = new Firebase("https://firebutton.firebaseio-demo.com/button");
+
+board.on("ready", function () {
+  var servo = new five.Servo(10);
+  var button = new five.Button(13);
+  //var buttonLed = new five.Led(8);
+servo.sweep();
+  
+  button.on("up", function () {
+    firebaseRef.set(false);
+  });
+
+  button.on("down", function () {
+    firebaseRef.set(true);
+  });
+
+  firebaseRef.on("value", function (snapshot) {
+    var buttonValue = snapshot.val();
+    if (buttonValue) {
+      console.log("sweeping servo");
+      servo.sweep();
+      //servo.to(90);
+      //setTimeout(function() {
+      //  servo.to(80);
+      //}, 500);
+    } 
+  });
+});
